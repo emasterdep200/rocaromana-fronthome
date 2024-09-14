d760d58a09e94097ba4adec87693c03c05b638f5
Luis Toscano <altrixdao@protonmail.com>
2024-09-14 10:00:44 -0500

    sss

--- src/Components/AddPropertyTabs/AddPropertyTabs.jsx
+++ src/Components/AddPropertyTabs/AddPropertyTabs.jsx
@@ -783,18 +783,7 @@ export default function AddPropertyTabs() {
                                             </select>
                                         </div>
                                     </div>
-                                    {/* <div className="col-sm-12 col-md-6 col-lg-4">
-                                        <div className="add_prop_fields">
-                                            <span>{translate("propStatus")}</span>
-                                            <span class="switch mt-2">
-                                                <input id="switch-rounded"
-                                                    type="checkbox"
-                                                    checked={tab1.isPrivate}
-                                                    onChange={handleToggleChange} />
-                                                <label for="switch-rounded"></label>
-                                            </span>
-                                        </div>
-                                    </div> */}
+
 
                                 </div>
                             )}
