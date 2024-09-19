a4d45a4902f24597d480e454ac17bc959c8cac7d
Luis Toscano <altrixdao@protonmail.com>
2024-09-19 16:19:33 -0500

    fixing.

---
+++ pages/user/publicidad/index.jsx
@@ -0,0 +1,22 @@
+"use client"
+import Meta from '@/Components/Seo/Meta'
+import React from 'react'
+import UserPublicidad from '@/Components/User/UserPublicidad.jsx'
+
+
+const Index = () => {
+    return (
+        <>
+            <Meta
+                title=""
+                description=""
+                keywords=""
+                ogImage=""
+                pathName=""
+            />
+            <UserPublicidad />
+        </>
+    )
+}
+
+export default Index
