f4e46a909c7b3e332812c6f2be00fe21669627dd
Luis Toscano <altrixdao@protonmail.com>
2024-09-25 00:51:34 -0500

    sssss

--- src/Components/User/UserPublicidad.jsx
+++ src/Components/User/UserPublicidad.jsx
@@ -113,10 +113,7 @@ const UserPublicidad = () => {
                                             <TableCell component="th" scope="row" sx={{ width: "40%" }}>
                                                 <div className="card" id="listing_card">
                                                     <div className="listing_card_img">
-                                                        <Image loading="lazy" src={'https://admin.rocaromana.com/images/publicity/' + elem?.imagen} alt="no_img" id="main_listing_img" width={200} height={240} style={{ height: "auto" }} onError={placeholderImage} />
-                                                        <span className="listing_type_feature_tag">
-                                                            <FaCrown />
-                                                        </span>
+                                                        <Image loading="lazy" src={'https://admin.rocaromana.com/images/publicity/' + elem?.imagen} alt="no_img" id="main_listing_img" width={200} height={240} onError={placeholderImage} />
                                                     </div>
                                                     <div className="listing_card_body">
                                                         <span className="listing_prop_title">{elem?.titulo}</span>
